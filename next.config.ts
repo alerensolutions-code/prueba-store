2ac9765490bb2cfaed509b6ce3a42d61480e5816
Agustina Guerendiain <agusguere@gmail.com>
2026-05-07 14:23:20 +0200

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.aleren.com.ar',
-        pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'https',
